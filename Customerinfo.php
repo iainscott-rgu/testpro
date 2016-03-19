2ba6b87b8ae38bede75c0a025b74ee9c4109543c
Iain Scott <odyssey3007@hotmail.com>
2016-03-19 20:45:01 +0000

    yip!

--- Customerinfo.php
+++ Customerinfo.php
@@ -1,50 +1,7 @@
 <html>
 
 
-
-<?php
-/**
- * This example shows making an SMTP connection with authentication.
- */
-echo "here is a message";
-
-
-/**
- * This example shows how to make use of PHPMailer's exceptions for error handling.
- */
-require 'PHPMailerAutoload.php';
-//Create a new PHPMailer instance
-//Passing true to the constructor enables the use of exceptions for error handling
-$mail = new PHPMailer(true);
-try {
-    //Set who the message is to be sent from
-    $mail->setFrom('from@example.com', 'First Last');
-    //Set an alternative reply-to address
-    $mail->addReplyTo('replyto@example.com', 'First Last');
-    //Set who the message is to be sent to
-    $mail->addAddress('whoto@example.com', 'John Doe');
-    //Set the subject line
-    $mail->Subject = 'PHPMailer Exceptions test';
-    //Read an HTML message body from an external file, convert referenced images to embedded,
-    //and convert the HTML into a basic plain-text alternative body
-    $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-    //Replace the plain text body with one created manually
-    $mail->AltBody = 'This is a plain-text message body';
-    //Attach an image file
-    $mail->addAttachment('images/phpmailer_mini.png');
-    //send the message
-    //Note that we don't need check the response from this because it will throw an exception if it has trouble
-    $mail->send();
-    echo "Message sent!";
-} catch (phpmailerException $e) {
-    echo $e->errorMessage(); //Pretty error messages from PHPMailer
-} catch (Exception $e) {
-    echo $e->getMessage(); //Boring error messages from anything else!
-}
-
-?>
-
-
+<body>
 
 
 <section class="spacer" id="spacer">
@@ -63,7 +20,7 @@ try {
 
 <section class="container" id="content2">
 
-    <form>
+    <form action=""test.php" method="post">
 
         <table class="table6">
 
@@ -97,22 +54,7 @@ try {
             <tr>
                 <td colspan="4"><p align="right" ><input class="btn2" type="submit" value="Submit" class="submit" />
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    </p></td>
+                  </p></td>
             </tr>
         </table></form>
 </section>
@@ -140,5 +82,5 @@ try {
     </div>
 
 </section>
-
+</body>
 </html>
