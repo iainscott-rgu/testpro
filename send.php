c9d7bab1585b280d41988cad2328cfea6fc0a752
Iain Scott <odyssey3007@hotmail.com>
2016-03-20 03:51:21 +0000

    yip!

--- send.php
+++ send.php
@@ -24,9 +24,9 @@ $mail->Body = 'Name: '.$title.' '.$firstname.' '.$surname."\n".'Your Email: '.$e
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-    echo "Welcome to the Booking Confirmation Page!!!";
-    echo "\n";
-    echo "A confirmation email has been sent to the address provided!";
+    echo "Welcome to the Booking Confirmation Page!!!".PHP_EOL;
+    echo "".PHP_EOL;
+    echo "A confirmation email has been sent to the address provided!".PHP_EOL;
 }
 
 ?>
