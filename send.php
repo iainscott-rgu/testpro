272f6db86462e7c4653d0fc1b218588f1995cc2a
Iain Scott <odyssey3007@hotmail.com>
2016-03-20 01:37:21 +0000

    yip!

--- send.php
+++ send.php
@@ -24,14 +24,14 @@ $mail->Password = "Pedro123";
 //Set who the message is to be sent from
 $mail->setFrom('thebnbhub@outlook.com');
 //Set who the message is to be sent to
-$mail->addAddress('iscott3007@gmail.com', ''.$firstname.$surname);
+$mail->addAddress('iscott3007@gmail.com'.''.$firstname.''.$surname);
 //Set the subject line
 $mail->Subject = 'PHPMailer SMTP test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
 $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
-$mail->Body = 'This is a plain-text message body /n'. $email;
+$mail->Body = 'This is a plain-text message body /n'.$email;
 //Attach an image file
 $mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
