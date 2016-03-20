a3b6fe780e1d629b5df9eaf2a3506ddca724e73d
Iain Scott <odyssey3007@hotmail.com>
2016-03-20 01:22:41 +0000

    yip!

--- send.php
+++ send.php
@@ -1,8 +1,10 @@
 <?php
 
-$name = $_POST['name'];
-$request = $_POST['request'];
+$title = $_POST['title'];
+$firstname = $_POST['firstname'];
+$surname = $_POST['surname'];
 $email = $_POST['email'];
+$telephone = $_POST['telephone'];
 
 require 'PHPMailerAutoload.php';
 //Create a new PHPMailer instance
@@ -20,17 +22,16 @@ $mail->Username = "thebnbhub@outlook.com";
 //Password to use for SMTP authentication
 $mail->Password = "Pedro123";
 //Set who the message is to be sent from
-$mail->setFrom('thebnbhub@outlook.com', 'First Last');
-
+$mail->setFrom('thebnbhub@outlook.com');
 //Set who the message is to be sent to
-$mail->addAddress('[$email]', 'John Doe');
+$mail->addAddress('iscott@outlook.com'.$firstname.$surname);
 //Set the subject line
 $mail->Subject = 'PHPMailer SMTP test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
 $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
-$mail->Body = 'This is a plain-text message body'. $email;
+//$mail->Body = 'This is a plain-text message body /n'. $email;
 //Attach an image file
 $mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
