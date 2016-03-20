51805fce28e1214bbab2428d84ed8ddc3bbd0f7d
Iain Scott <odyssey3007@hotmail.com>
2016-03-20 01:24:00 +0000

    yip!

--- send.php
+++ send.php
@@ -31,7 +31,7 @@ $mail->Subject = 'PHPMailer SMTP test';
 //convert HTML into a basic plain-text alternative body
 $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
-//$mail->Body = 'This is a plain-text message body /n'. $email;
+$mail->Body = 'This is a plain-text message body /n'. $email;
 //Attach an image file
 $mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
