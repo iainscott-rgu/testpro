403fef3e4d104d2ec744cd76c17bfa67da155d03
Iain Scott <odyssey3007@hotmail.com>
2016-03-20 01:46:15 +0000

    yip!

--- send.php
+++ send.php
@@ -7,33 +7,23 @@ $email = $_POST['email'];
 $telephone = $_POST['telephone'];
 
 require 'PHPMailerAutoload.php';
-//Create a new PHPMailer instance
 $mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
 $mail->isSMTP();
-//Set the hostname of the mail server
 $mail->Host = "smtp.live.com";
-//Set the SMTP port number - likely to be 25, 465 or 587
 $mail->Port = 25;
-//Whether to use SMTP authentication
 $mail->SMTPAuth = true;
-//Username to use for SMTP authentication
 $mail->Username = "thebnbhub@outlook.com";
-//Password to use for SMTP authentication
 $mail->Password = "Pedro123";
-//Set who the message is to be sent from
 $mail->setFrom('thebnbhub@outlook.com');
-//Set who the message is to be sent to
-$mail->addAddress('iscott3007@gmail.com'.''.$firstname.''.$surname);
-//Set the subject line
-$mail->Subject = 'PHPMailer SMTP test';
+$mail->addAddress('iscott3007@gmail.com');
+$mail->Subject = 'Booking Confirmation';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+$mail->msgHTML(file_get_contents('contents.html'), dirname(contents.html));
 //Replace the plain text body with one created manually
-$mail->Body = 'This is a plain-text message body /n'.$email;
+$mail->Body = 'This is a plain-text message body';
 //Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
+//$mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
